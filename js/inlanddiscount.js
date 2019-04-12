16c79dd2e33cf6927d612fc4bb0a0a0f7b950607
zxm <15219600748@163.com>
2019-04-12 15:24:53 +0800

    国内折扣完成

--- js/inlanddiscount.js
+++ js/inlanddiscount.js
@@ -1,9 +1,9 @@
 $(function () {
-    mui('.mui-scroll-wrapper').scroll({
-        deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-        indicators: false, //是否显示滚动条
-        bounce: false//是否启用回弹
+    mui('body').on('tap','a',function(){
+        window.top.location.href=this.href;
+    
     });
+
     mui.init({
         pullRefresh: {
             container: "#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
@@ -38,6 +38,11 @@ $(function () {
                 var goodsListHtml = template('goodsListTemp', result)
                 // console.log(goodsListHtml);
                 $('.goods_list').html(goodsListHtml)
+                mui('.mui-scroll-wrapper').scroll({
+                    deceleration: 0.0006, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+                    indicators: false, //是否显示滚动条
+                    bounce: false//是否启用回弹
+                });
 
             }
         })
